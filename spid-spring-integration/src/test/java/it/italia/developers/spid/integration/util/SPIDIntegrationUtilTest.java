1934e9e54b6c66a408d612e919f19298daa05957
Luca Castelluzzo <lucastle@gmail.com>
2017-10-07 20:30:46 +0200

    Risolto conflitto

--- spid-spring-integration/src/test/java/it/italia/developers/spid/integration/util/SPIDIntegrationUtilTest.java
+++ spid-spring-integration/src/test/java/it/italia/developers/spid/integration/util/SPIDIntegrationUtilTest.java
@@ -2,14 +2,16 @@ package it.italia.developers.spid.integration.util;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.Scanner;
-
+import javax.xml.parsers.ParserConfigurationException;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-
+import org.w3c.dom.Element;
+import org.xml.sax.SAXException;
 import it.italia.developers.spid.integration.Application;
 import it.italia.developers.spid.integration.exception.IntegrationServiceException;
 import junit.framework.Assert;
@@ -18,36 +20,23 @@ import junit.framework.Assert;
 @ContextConfiguration(classes = { Application.class })
 public class SPIDIntegrationUtilTest {
 
-	@Autowired
-	private SPIDIntegrationUtil spidIntegrationUtil;
+      @Autowired
+      private SPIDIntegrationUtil spidIntegrationUtil;
 
-	@Test
-	public void xmlStringToXMLObjectTest() {
-		try {
-<<<<<<< HEAD
-            ClassLoader classLoader = getClass().getClassLoader();
-            File xmlFile = new File(classLoader.getResource("telecom-metadata.xml").getFile());
-            String xmlData = new Scanner(xmlFile).useDelimiter("\\Z").next();            
-            spidIntegrationUtil.xmlStringToElement(xmlData);
-            
-		} catch (SAXException | IOException | ParserConfigurationException e) {
-            e.printStackTrace();
-            Assert.fail();
-=======
-			ClassLoader classLoader = getClass().getClassLoader();
-			File xmlFile = new File(classLoader.getResource("telecom-metadata.xml").getFile());
-			String xmlData = new Scanner(xmlFile).useDelimiter("\\Z").next();
-			spidIntegrationUtil.xmlStringToXMLObject(xmlData);
+      @Test
+      public void xmlStringToXMLObjectTest() {
 
-		}
-		catch (IntegrationServiceException e) {
-			e.printStackTrace();
-			Assert.fail();
-		}
-		catch (FileNotFoundException e) {
-			e.printStackTrace();
-			Assert.fail();
->>>>>>> 56111b8aaca2a4a11af9e9a43e6e844895b1e7bc
-		}
-	}
+            try {
+                  ClassLoader classLoader = getClass().getClassLoader();
+                  File xmlFile = new File(classLoader.getResource("metadata/idp/telecom-metadata.xml").getFile());
+                  String xmlData = new Scanner(xmlFile).useDelimiter("\\Z").next();
+                  Element node = spidIntegrationUtil.xmlStringToElement(xmlData);
+
+                  Assert.assertEquals("md:EntityDescriptor", node.getNodeName());
+
+            } catch (SAXException | IOException | ParserConfigurationException e) {
+                  e.printStackTrace();
+                  Assert.fail();
+            }
+      }
 }
