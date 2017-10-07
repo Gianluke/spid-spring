f389f7b5174b3f5cb02b0e02e2d31f798f277cdc
luca <luca@VPWK002-LE>
2017-10-07 13:58:55 +0200

    Primo commit.

---
+++ spid-spring-integration/src/main/java/org/spid/spring/integration/App.java
@@ -0,0 +1,13 @@
+package org.spid.spring.integration;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ spid-spring-rest/src/main/java/it/italia/developers/spid/spidspringrest/SpidSpringRestApplication.java
@@ -0,0 +1,12 @@
+package it.italia.developers.spid.spidspringrest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpidSpringRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpidSpringRestApplication.class, args);
+	}
+}
